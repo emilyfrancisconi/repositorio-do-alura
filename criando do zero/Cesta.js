3f65eefa9166dcfab52b639cbaa754b74e26d872
emilyfrancisconi <francisconi.emily@escola.pr.gov.br>
2026-03-24 10:36:44 -0300

    Add files via upload

---
+++ criando do zero/Cesta.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
+
+import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
+
+const width = Dimensions.get('screen').width;
+
+export default function Cesta() {
+  return <>
+    <Image source={topo} style={estilos.topo} />
+    <Text style={estilos.titulo}>Detalhe da cesta</Text>
+
+    <View style={estilos.cesta}>
+      <Text style={estilos.nome}>Cesta de Verduras</Text>
+      <View style={estilos.fazenda}>
+        <Image source={logo} style={estilos.imagemFazenda} />
+        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+      </View>
+      <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+      <Text style={estilos.preco}>R$ 40,00</Text>
+    </View>
+  </>
+}
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: 578 / 768 * width,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    fontWeight: "bold",
+    padding: 16,
+  },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontFamily: "MontserratBold",
+  },
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imagemFazenda: {
+    width: 32,
+    height: 32,
+  },
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: "MontserratRegular",
+  },
+  descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  preco: {
+    color: "#2A9F85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+  },
+});
+
+
+import logo from '../../assets/logo.png';
+
+const cesta = {
+  topo: {
+    titulo: "Detalhe da cesta",
+  },
+  detalhes: {
+    nome: "Cesta de Verduras",
+    logoFazenda: logo,
+    nomeFazenda: "Jenny Jack Farm",
+    descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+    preco: "R$ 40,00",
+  }
+}
+
+export default cesta;
+
+import logo from '../../assets/logo.png';
+
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
+
+const cesta = {
+  topo: {
+    titulo: "Detalhe da cesta",
+  },
+  detalhes: {
+    nome: "Cesta de Verduras",
+    logoFazenda: logo,
+    nomeFazenda: "Jenny Jack Farm",
+    descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+    preco: "R$ 40,00",
+    botao: "Comprar",
+  },
+  itens: {
+    titulo: "Itens da cesta",
+    lista: [
+      {
+        nome: "Tomate",
+        imagem: tomate,
+      },
+      {
+        nome: "Brócolis",
+        imagem: brocolis,
+      },
+      {
+        nome: "Batata",
+        imagem: batata,
+      },
+      {
+        nome: "Pepino",
+        imagem: pepino,
+      },
+      {
+        nome: "Abóbora",
+        imagem: abobora,
+      }
+    ]
+  }
+}
+
+export default cesta;
